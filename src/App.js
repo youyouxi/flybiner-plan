cd6c79f1fe155149714eaa4bbd4202fd0e676270
flybiner <flybiner@gmail.com>
2024-05-23 09:06:16 +0800

    集成 ant design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Button, Flex } from 'antd';
+import {Col, Row} from 'antd';
+
 
 const App: React.FC = () => (
-    <Flex gap="small" wrap>
-        <Button type="primary">Primary Button</Button>
-        <Button>Default Button</Button>
-        <Button type="dashed">Dashed Button</Button>
-        <Button type="text">Text Button</Button>
-        <Button type="link">Link Button</Button>
-    </Flex>
+    <Row>
+        <Col span={18} style={{background: "white"}}>col-8</Col>
+        <Col span={6} style={{background: "white"}}>col-8</Col>
+    </Row>
 );
 
 export default App;
